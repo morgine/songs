dcb18d71b34343e7db675b2a233b1592dab21f04
morgine <ksnmmmm@gmail.com>
2020-12-01 18:13:06 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,9 @@
   </div>
 </template>
 <script>
-import Qs from 'qs'
 export default {
   name: 'App',
   created () {
-    this.$axios({
-      baseURL: process.env.API,
-      paramsSerializer (params) {
-        return Qs.stringify(params, { indices: false })
-      }
-    })
     const token = localStorage.getItem('Authorization')
     if (token) {
       this.$axios.defaults.headers.common.Authorization = token
--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,17 @@
 import Vue from 'vue'
 import axios from 'axios'
+import Qs from 'qs'
 
-Vue.prototype.$axios = axios
+const $axios = axios.create({
+  baseURL: process.env.API,
+  paramsSerializer (params) {
+    return Qs.stringify(params, { indices: false })
+  }
+})
+
+const token = localStorage.getItem('Authorization')
+if (token) {
+  $axios.defaults.headers.common.Authorization = token
+}
+
+Vue.prototype.$axios = $axios
