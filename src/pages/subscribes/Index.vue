740fab117475dff3173d0ac95a097074e817fadf
petter <ksnmmmm@gmail.com>
2020-12-11 00:37:03 +0800

    first commit

--- src/pages/subscribes/Index.vue
+++ src/pages/subscribes/Index.vue
@@ -1,32 +1,94 @@
 <template>
   <q-page padding>
-    <q-form class="column q-gutter-lg">
-      <q-input label="小程序标题" v-model="card.Title"/>
-      <q-input label="小程序地址" v-model="card.PagePath"/>
-      <div>
-        <a href="javascript: void(0)" @click="$refs['image-selector'].openDialog()">
-          <q-img
-            :src="card.ThumbMediaFilename?`${$axios.defaults.baseURL}/picture/${card.ThumbMediaFilename}`:''"
-            width="100px"
-            height="120px"
-          >
-            <template v-slot:error>
-              <div class="absolute-full flex flex-center">
-                选择封面
-              </div>
-            </template>
-            <div class="absolute-full text-subtitle1 flex flex-center">
-              选择封面
-            </div>
-          </q-img>
-        </a>
-      </div>
-      <div class="row">
-        <q-btn label="删除" @click="delCard"/>
-        <q-btn label="保存" color="primary" class="q-ml-md" @click="saveCard"/>
-      </div>
-    </q-form>
-    <ImageSelector v-model="card.ThumbMediaFilename" ref="image-selector"></ImageSelector>
+    <q-card flat>
+      <q-card-section>
+        <div class="text-h6">图文列表</div>
+      </q-card-section>
+      <q-card-section>
+        <q-card flat v-for="(article, idx) in articles" :key="idx">
+          <q-card-section>
+            <q-input label="标题" v-model="article.Title"/>
+          </q-card-section>
+          <q-card-section>
+            <q-input label="描述" v-model="article.Description"/>
+          </q-card-section>
+          <q-card-section>
+            <q-input label="地址" v-model="article.Url"/>
+          </q-card-section>
+          <q-card-section>
+            <a href="javascript: void(0)" @click="$refs[`article-image-selector-${idx}`].openDialog()">
+              <q-img
+                :src="article.PicFile?`${$axios.defaults.baseURL}/picture/${article.PicFile}`:''"
+                :width="idx===0?'300px':'100px'"
+                height="120px"
+                class="q-hoverable"
+              >
+                <template v-slot:error>
+                  <div class="absolute-full flex flex-center">
+                    选择封面
+                  </div>
+                </template>
+                <div class="absolute-full text-subtitle1 flex flex-center">
+                  选择封面
+                </div>
+              </q-img>
+            </a>
+            <ImageSelector v-model="article.PicFile" :ref="`article-image-selector-${idx}`"></ImageSelector>
+          </q-card-section>
+          <q-card-actions align="around">
+            <q-btn icon="delete" @click="articles.splice(idx, 1)"/>
+            <q-btn icon="plus" color="primary" class="q-ml-md" @click="articles.splice(idx, 0, initArticle())"/>
+          </q-card-actions>
+        </q-card>
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn icon="delete" @click="delArticles" color="warning"/>
+        <q-btn icon="check" @click="saveArticles" color="primary"/>
+      </q-card-actions>
+    </q-card>
+    <q-card flat>
+      <q-card-section>
+        <div class="text-h6">小程序卡片</div>
+      </q-card-section>
+      <q-card-section>
+        <q-card flat v-for="(card, idx) in cards" :key="idx">
+          <q-card-section>
+            <q-input label="小程序标题" v-model="card.Title"/>
+          </q-card-section>
+          <q-card-section>
+            <q-input label="小程序地址" v-model="card.PagePath"/>
+          </q-card-section>
+          <q-card-section>
+            <a href="javascript: void(0)" @click="$refs[`image-selector-${idx}`].openDialog()">
+              <q-img
+                :src="card.ThumbMediaFilename?`${$axios.defaults.baseURL}/picture/${card.ThumbMediaFilename}`:''"
+                width="100px"
+                height="120px"
+                class="q-hoverable"
+              >
+                <template v-slot:error>
+                  <div class="absolute-full flex flex-center">
+                    选择封面
+                  </div>
+                </template>
+                <div class="absolute-full text-subtitle1 flex flex-center">
+                  选择封面
+                </div>
+              </q-img>
+            </a>
+            <ImageSelector v-model="card.ThumbMediaFilename" :ref="`image-selector-${idx}`"></ImageSelector>
+          </q-card-section>
+          <q-card-actions align="around">
+            <q-btn icon="delete" @click="cards.splice(idx, 1)"/>
+            <q-btn icon="plus" color="primary" class="q-ml-md" @click="cards.splice(idx, 0, initCard())"/>
+          </q-card-actions>
+        </q-card>
+      </q-card-section>
+      <q-card-actions align="around">
+        <q-btn icon="delete" @click="delCard" color="warning"/>
+        <q-btn icon="check" @click="saveCard" color="primary"/>
+      </q-card-actions>
+    </q-card>
   </q-page>
 </template>
 
@@ -38,28 +100,40 @@ export default {
   components: { ImageSelector },
   data () {
     return {
-      card: {
-        Title: '',
-        PagePath: '',
-        ThumbMediaFilename: ''
+      cards: [],
+      initCard () {
+        return {
+          Title: '',
+          PagePath: '',
+          ThumbMediaFilename: ''
+        }
       },
-      dialog: false
+      articles: [],
+      initArticle () {
+        return {
+          Title: '',
+          Description: '',
+          Url: '',
+          PicFile: ''
+        }
+      }
     }
   },
   created () {
     this.getCard()
+    this.getArticles()
   },
   methods: {
     getCard () {
       this.$axios.get('/mini-program-card').then(data => {
         data = data.data
         if (data && data.Data) {
-          this.card = data.Data
+          this.cards = data.Data
         }
       })
     },
     saveCard () {
-      this.$axios.put('/mini-program-card', this.card).then(data => {
+      this.$axios.put('/mini-program-card', this.cards).then(data => {
         data = data.data
         if (data && data.Message) {
           this.$q.notify(data.Message)
@@ -67,16 +141,51 @@ export default {
       })
     },
     delCard () {
-      this.$axios.delete('/mini-program-card').then(data => {
+      this.$q.dialog({
+        title: '⚠️ 警告',
+        message: '删除所有小程序卡片?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.$axios.delete('/mini-program-card').then(data => {
+          data = data.data
+          if (data && data.Message) {
+            this.$q.notify(data.Message)
+            this.cards = []
+          }
+        })
+      })
+    },
+    getArticles () {
+      this.$axios.get('/articles').then(data => {
+        data = data.data
+        if (data && data.Data) {
+          this.articles = data.Data
+        }
+      })
+    },
+    saveArticles () {
+      this.$axios.put('/articles', this.articles).then(data => {
         data = data.data
         if (data && data.Message) {
           this.$q.notify(data.Message)
-          this.card = {
-            Title: '',
-            PagePath: '',
-            ThumbMediaFilename: ''
-          }
         }
+      })
+    },
+    delArticles () {
+      this.$q.dialog({
+        title: '⚠️ 警告',
+        message: '删除所有图文消息?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.$axios.delete('/articles').then(data => {
+          data = data.data
+          if (data && data.Message) {
+            this.$q.notify(data.Message)
+            this.articles = []
+          }
+        })
       })
     }
   }
